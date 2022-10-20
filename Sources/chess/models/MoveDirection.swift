dce3eaeaf78cc8aab954ae6365e8a5a1ffb88f68
tomieq <tomieq@wp.pl>
2022-10-20 17:10:41 +0200

    Implemented MoveDirection

---
+++ Sources/chess/models/MoveDirection.swift
@@ -0,0 +1,15 @@
+//
+//  MoveDirection.swift
+//
+//
+//  Created by Tomasz on 11/09/2022.
+//
+
+import Foundation
+
+enum MoveDirection {
+    case right
+    case left
+    case up
+    case down
+}
