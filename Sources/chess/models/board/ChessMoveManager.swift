a95d5e77a3fefe108a0b3cff6c1ec7126a9641e8
tomieq <tomieq@wp.pl>
2024-08-05 20:01:42 +0200

    Implemented castling

--- Sources/chess/models/board/ChessMoveManager.swift
+++ Sources/chess/models/board/ChessMoveManager.swift
@@ -38,6 +38,43 @@ public class ChessMoveManager {
             print("\(piece) cannot move to \(to). It can move only to \(piece.moveCalculator.possibleMoves)")
             throw ChessMoveError.canNotMove(to: to)
         }
+        defer {
+            colorOnMove = colorOnMove.other
+        }
+        if piece.type == .king, piece.moveCalculator.moveCounter == 0 {
+            switch piece.color {
+            case .white:
+                if to == "g1", let rook = chessboard["h1"] {
+                    // short castling
+                    updateChessboardWithMove(piece: piece, from: "e1", to: "g1")
+                    updateChessboardWithMove(piece: rook, from: "h1", to: "f1")
+                    share(.pieceMoved(from: "e1", to: "g1"))
+                    share(.pieceMoved(from: "h1", to: "f1"))
+                }
+                if to == "c1", let rook = chessboard["a1"] {
+                    // short castling
+                    updateChessboardWithMove(piece: piece, from: "e1", to: "c1")
+                    updateChessboardWithMove(piece: rook, from: "a1", to: "d1")
+                    share(.pieceMoved(from: "e1", to: "c1"))
+                    share(.pieceMoved(from: "a1", to: "d1"))
+                }
+            case .black:
+                if to == "g8", let rook = chessboard["h8"] {
+                    // short castling
+                    updateChessboardWithMove(piece: piece, from: "e8", to: "g8")
+                    updateChessboardWithMove(piece: rook, from: "h8", to: "f8")
+                    share(.pieceMoved(from: "e8", to: "g8"))
+                    share(.pieceMoved(from: "h8", to: "f8"))
+                }
+                if to == "c8", let rook = chessboard["a8"] {
+                    // short castling
+                    updateChessboardWithMove(piece: piece, from: "e8", to: "c8")
+                    updateChessboardWithMove(piece: rook, from: "a8", to: "d8")
+                    share(.pieceMoved(from: "e8", to: "c8"))
+                    share(.pieceMoved(from: "a8", to: "d8"))
+                }
+            }
+        }
         let event: ChessBoardEvent!
         if let attackedPiece = chessboard[to] {
             event = .pieceTakes(from: from, to: to, killedType: attackedPiece.type)
@@ -46,11 +83,18 @@ public class ChessMoveManager {
             event = .pieceMoved(from: from, to: to)
             print("\(piece) moved from \(from) to \(to)")
         }
+        updateChessboardWithMove(piece: piece, from: from, to: to)
+        share(event)
+    }
+    
+    private func share(_ event: ChessBoardEvent) {
+        status?(event)
+        chessboard.broadcast(event: event)
+    }
+
+    private func updateChessboardWithMove(piece: ChessPiece, from: BoardSquare, to: BoardSquare) {
         let movedPiece = piece.moved(to: to)
         chessboard.pieces.removeAll { [to, from].contains($0.square) }
         chessboard.pieces.append(movedPiece)
-        status?(event)
-        chessboard.broadcast(event: event)
-        colorOnMove = colorOnMove.other
     }
 }
