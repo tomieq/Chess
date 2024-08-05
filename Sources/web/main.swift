a9b80e513837c45b7984e575edebe6643ab039f8
tomieq <tomieq@wp.pl>
2024-08-05 12:05:18 +0200

    Added chessboard UI

--- Sources/web/main.swift
+++ Sources/web/main.swift
@@ -1,3 +1,23 @@
 import Foundation
+import Swifter
+import Template
 
-print("Server started")
+
+
+do {
+    let server = HttpServer()
+    server["/"] = { request, headers in
+        let template = Template.load(relativePath: "templates/index.html")
+        return .ok(.html(template))
+    }
+    server.notFoundHandler = { request, responseHeaders in
+        let resourcePath = Resource().absolutePath(for: request.path)
+        try HttpFileResponse.with(absolutePath: resourcePath)
+        return .notFound()
+    }
+    try server.start(8080)
+    print("Server started on port \(try server.port())")
+    dispatchMain()
+} catch {
+    print(error)
+}
