9d8f4bfb97c10e883dcde4136db8bb36f6e9d2d8
tomieq <tomieq@wp.pl>
2024-08-06 16:22:08 +0200

    Implemented parsing castling

--- Sources/chess/parsers/NotationParser.swift
+++ Sources/chess/parsers/NotationParser.swift
@@ -30,37 +30,52 @@ public class NotationParser {
     public func apply(_ txt: String) throws -> [ChessMoveEvent] {
         var events: [ChessMoveEvent] = []
         let parts = split(txt)
+        let color = moveManager.colorOnMove
         for part in parts {
-            var type: ChessPieceType?
-            var to: BoardSquare?
-            let takes = part.contains("x")
-            let part = part.replacingOccurrences(of: "x", with: "")
-            if part.count == 2 {
-                type = .pawn
-                to = BoardSquare(stringLiteral: part)
-            } else {
-                to = BoardSquare(stringLiteral: part.subString(1, 3))
-                type = ChessPieceType.make(letter: part.subString(0, 1), language: language)
-            }
-            guard let type = type, let to = to else {
-                throw NotationParserError.parsingError("Invalid entry \(part)")
-            }
-            let pieces = moveManager.chessboard
-                .getPieces(color: moveManager.colorOnMove)
-                .filter { $0.type == type }
-                .filter { $0.moveCalculator.possibleMoves.contains(to)}
-            guard pieces.count == 1, let piece = pieces.first else {
-                throw NotationParserError.parsingError("Ambigious entry \(part)")
-            }
-            let move = ChessMove(from: piece.square, to: to)
-            let event = takes ?  ChessMoveEvent.pieceTakes(type: type, move: move, takenType: moveManager.chessboard[to]?.type ?? .pawn) : ChessMoveEvent.pieceMoved(type: type, move: move)
+            let event = try parseCastling(part) ?? parseSingleMove(part)
             moveManager.consume(event)
             events.append(event)
-            if moveManager.chessboard.isCheckMate(for: piece.color) {
-                events.append(.checkMate(piece.color))
-                moveManager.consume(.checkMate(piece.color))
+            if moveManager.chessboard.isCheckMate(for: color) {
+                events.append(.checkMate(color))
+                moveManager.consume(.checkMate(color))
             }
         }
         return events
+    }
+    
+    private func parseCastling(_ part: String) -> ChessMoveEvent? {
+        let color = moveManager.colorOnMove
+        if part == "O-O" || part == "0-0"{
+            return .castling(side: Castling.kingSide(color))
+        } else if part == "O-O-O" || part == "0-0-0"{
+            return .castling(side: Castling.queenSide(color))
+        }
+        return nil
+    }
+
+    private func parseSingleMove(_ part: String) throws -> ChessMoveEvent {
+        var type: ChessPieceType?
+        var to: BoardSquare?
+        let takes = part.contains("x")
+        let part = part.replacingOccurrences(of: "x", with: "")
+        if part.count == 2 {
+            type = .pawn
+            to = BoardSquare(stringLiteral: part)
+        } else {
+            to = BoardSquare(stringLiteral: part.subString(1, 3))
+            type = ChessPieceType.make(letter: part.subString(0, 1), language: language)
+        }
+        guard let type = type, let to = to else {
+            throw NotationParserError.parsingError("Invalid entry \(part)")
+        }
+        let pieces = moveManager.chessboard
+            .getPieces(color: moveManager.colorOnMove)
+            .filter { $0.type == type }
+            .filter { $0.moveCalculator.possibleMoves.contains(to)}
+        guard pieces.count == 1, let piece = pieces.first else {
+            throw NotationParserError.parsingError("Ambigious entry \(part)")
+        }
+        let move = ChessMove(from: piece.square, to: to)
+        return takes ?  ChessMoveEvent.pieceTakes(type: type, move: move, takenType: moveManager.chessboard[to]?.type ?? .pawn) : ChessMoveEvent.pieceMoved(type: type, move: move)
     }
 }
